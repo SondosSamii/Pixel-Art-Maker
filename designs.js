8dd8468ad9d09c84746b587b8d21bac510c4b106
SondosSamii <sondos379@gmail.com>
2019-10-24 23:09:15 +0200

    feat: game implementation

--- designs.js
+++ designs.js
@@ -1,9 +1,53 @@
 // Select color input
 // Select size input
 
-// When size is submitted by the user, call makeGrid()
+const ui = {
+    sizePicker: document.getElementById("sizePicker"),
+    pixelCanvas: document.getElementById("pixelCanvas"),
+    colorPicker: document.getElementById("colorPicker"),
+    inputHeight: document.getElementById("inputHeight"),
+    inputWidth: document.getElementById("inputWidth"),
+};
 
-function makeGrid() {
-  // Your code goes here!
+function cellOnClick() {
+    let color = ui.colorPicker.value;
+    this.style.backgroundColor = color;
 }
 
+function clearGrid() {
+    // Remove Event Listeners
+    for (let row of ui.pixelCanvas.rows) {
+        for (let cell of row.cells) {
+            cell.removeEventListener('click', cellOnClick);
+        }
+    }
+    // clear DOM
+    while (ui.pixelCanvas.hasChildNodes()) {
+        ui.pixelCanvas.removeChild(ui.pixelCanvas.firstChild);
+    }
+}
+
+function makeGrid() {
+    // Your code goes here!
+    let rows = ui.inputHeight.value;
+    let cols = ui.inputWidth.value;
+
+    for (let i = 0; i < rows; i++) {
+        let row = document.createElement('tr');
+        for (let i = 0; i < cols; i++) {
+            let cell = document.createElement('td');
+            row.appendChild(cell);
+            cell.addEventListener('click', cellOnClick)
+        }
+        ui.pixelCanvas.appendChild(row);
+    }
+}
+
+function sizePickerOnSubmit(e) {
+    e.preventDefault();
+    clearGrid();
+    makeGrid();
+}
+
+// When size is submitted by the user, call makeGrid()
+ui.sizePicker.addEventListener('submit', sizePickerOnSubmit);
